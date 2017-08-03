753d9c9ea368938776b3505bf91f042e4674e574
tifenn <tifenn@Debian.lan>
2017-08-03 11:32:23 +1000

    Add MySQL persistance data

--- create-images.sh
+++ create-images.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 docker build --file="docker/images/app/Dockerfile" --tag="app:latest" docker/images/app/.
-#docker build --file="docker/images/mysql/Dockerfile" --tag="dopsyng_mysql:latest" docker/images/mysql/.
+docker build --file="docker/images/mysql/Dockerfile" --tag="app_mysql:latest" docker/images/mysql/.
