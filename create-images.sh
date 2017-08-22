c80a4fd3d384632e24eb39370c6b28068ae47b18
tifenn <tifenn@Debian.lan>
2017-08-23 09:02:22 +1000

    Script.sh -> Makefile

--- create-images.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-docker build --file="docker/images/app/Dockerfile" --tag="app:latest" docker/images/app/.
-docker build --file="docker/images/mysql/Dockerfile" --tag="app_mysql:latest" docker/images/mysql/.
