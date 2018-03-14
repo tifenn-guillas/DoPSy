68414e431515b3fc88666c4450a1464f4e86c47d
Tifenn <tifenn.guillas@gmail.com>
2018-03-14 15:15:04 +1100

    Upgrade to Angular5

--- client/my-project/e2e/app.e2e-spec.ts
+++ client/my-project/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { MyProjectPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('my-project App', () => {
-  let page: MyProjectPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new MyProjectPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
